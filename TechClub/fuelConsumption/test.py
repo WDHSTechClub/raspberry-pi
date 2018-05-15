7706827c427bd7f196edb89bb774b33e4220bde8
WDHSTechClub <WDHSTechClub@gmail.com>
2018-05-14 20:47:52 -0500

    added engine stop at 3600 rpm

--- TechClub/fuelConsumption/test.py
+++ TechClub/fuelConsumption/test.py
@@ -9,10 +9,17 @@
 
 import csv
 import math
-import sys
 from arrays import *
 
 class SimulateRun:
+    """
+        Class used for modeling Go-Kart runs for WDHS Tech Club
+        
+        This class in used to run the simulate_run() subroutine
+        and has the purpose of optimizing fuel efficiency on
+        a real life go kart for WEEVA competitions for the Western
+        Dubuque High School racing team.
+    """
     
     def __init__(self):
         """
@@ -21,21 +28,20 @@ class SimulateRun:
             Defines all constants
             (Will add arguments later)
         """
-
+        
         # Constants
-        self._dLim = 2
-        self._spRatio = 85 / 12
-        self._kMass = 120
-        self._wheelDia = .5588
-        self._outputTorque = 6.9
-        self._dragCoefficent = .3
-        self._frontal = .56
-        self._airDensity = 1.225
-        self._forceTotal = 10
-        self._clutchSlip = 1.0
-        self._drivenWheelCir = math.pi * self._wheelDia
+        self._dLim = 2 # Decimal Delimiter
+        self._spRatio = 85 / 12 # Sprocket Ratio
+        self._kMass = 120 # Kart Mass
+        self._wheelDia = .5588 # Wheel Diameter
+        self._outputTorque = 6.9 # Engine Output Torque (Nm)
+        self._dragCoefficent = .3 # Coefficient of Drag
+        self._frontal = .56 # Kart Frontal Area (m^2)
+        self._airDensity = 1.225 # Air Density (constant)
+        self._forceTotal = 10 # Total of forces acting on kart
+        self._clutchSlip = 1.0 # Clutch slip as percentage
+        self._drivenWheelCir = math.pi * self._wheelDia # Driven Wheel Circumfrance
 
-    @classmethod
     def simulate_run(self, until:int, step:float, throttle:int)->bool:
         """
             Method to simulate a Go-Kart run
@@ -44,10 +50,8 @@ class SimulateRun:
             step:     time step (increment)
             throttle: throttle percentage divisible by 10
         """
-
-        if (type(until) != int or type(step) != float or type(throttle) != int):
-            raise TypeError('parameters must be integer, float, integer!')
-        else:
+        
+        try:
             fname = "run_" + str('%.3f' % step).split('.')[1] + "ms.txt"
 
             #Starting Values
@@ -59,10 +63,11 @@ class SimulateRun:
             clutchSprint = 0
             rpm = 2000
             timeSum = 0
-
+            lockup = False
+            bsfc = 0
 
             # Main Loop
-            for x in range(0, (int)(until * (1 / step) + 1)):
+            for x in range(0, (int)((until / step) + 1)):
 
                 # Calculated
                 kAccel = (((self._outputTorque * self._spRatio * 2) / self._wheelDia) - self._forceTotal - drag) / self._kMass # mph
@@ -71,6 +76,8 @@ class SimulateRun:
                 drag = (velSpeed ** 2) * self._airDensity * self._dragCoefficent * self._frontal / 2 # Drag Coefficient
                 clutchSpeed = velSpeed * 60 * self._spRatio / self._drivenWheelCir 
                 slip = (rpm - clutchSprint) / rpm
+                deltaBSFC = self._calcBSFC(int(rpm), int(throttle)) * self._calcPower(int(rpm), int(throttle)) * step
+                bsfc += deltaBSFC
 
                 # for slip < 0 we need to look up engine speeed using the clutchSpeed. Look up outputTorque == engine torque.   
                 # if lockup == true or
@@ -81,14 +88,10 @@ class SimulateRun:
                     rpm = clutchSpeed
 
                     # Lookup torque value
-                    torque = self._calcTorque(int(rpm), throttle)
-                    # print([int(rpm), throttle])
-                    # print(self._calcTorque(int(rpm), throttle))
-                    # print("sys torque: " + str(torque))
-
+                    torque = self._calcTorque(rpm, throttle)
 
                 # Output
-                output.append([round(timeSum, self._dLim), round(kAccel, self._dLim), round(velSpeed, self._dLim), round(dist, self._dLim), round(slip, self._dLim)])
+                output.append([round(timeSum, self._dLim), round(kAccel, self._dLim), round(velSpeed, self._dLim), round(dist, self._dLim), round(slip, self._dLim), round(bsfc, self._dLim)])
 
                 # Iterate Variables
                 velSprint = velSpeed
@@ -101,12 +104,15 @@ class SimulateRun:
             with open('runs/' + fname, 'w') as csvfile:
                 filewriter = csv.writer(csvfile, delimiter=',',
                                         quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                filewriter.writerow(["Time Step", "Kart Accel", "Vehicle Speed", "Total Distance", "Clutch Slip"])
+                filewriter.writerow(["Time Step", "Kart Accel", "Vehicle Speed", "Total Distance", "Clutch Slip", "BSFC"])
                 for iteration in output:
                     filewriter.writerow(iteration)
             return True
+        except Exception as e:
+            raise e
+            print(str(e))
+            return False
 
-    @classmethod
     def _calcTorque(self, rpm:int, throttle:int)->float:
         """
             Method to calculate torque
@@ -115,37 +121,62 @@ class SimulateRun:
             throttle: percentage divisible by 10
         """
 
-        if ((type(rpm) != int) or (type(throttle) != int)):
-            raise TypeError('Both parameters must be integers!')
+        # Ensure RPM is between 1400 and 3600
+        if (rpm >= 1400 and rpm <= 3600):
+
+            # Parse RPM as a string and strip decimal values
+            rString = str(int(rpm))
+            try:
+                # Only executes if no errors finding the "00" in rString
+                test = (rString.index("00") != 2)
+
+                # Gets torque at rpm if rpm is divisible by 100
+                return self._findTorque(rpm / 100, throttle)
+
+            except:
+                # The first two digits of RPM (used for torque lookup)
+                gets = int(rString[0:2])
+
+                # The last two digits of RPM (used for weighting torque multiplcation)
+                remain = int(rString[2:4])
+
+                # Find and weight the two torque values
+                t1 = self._findTorque(gets, throttle) * (float(100 - remain) / 100)
+                t2 = self._findTorque(gets + 1, throttle) * (float(remain) / 100)
+
+                # Return the sum of weighted torques
+                return t1 + t2
+        elif (rpm >= 3600):
+            return float(0)
         else:
-            # Ensure RPM is between 1400 and 3600
-            if (rpm >= 1400 and rpm <= 3600):
+            raise LookupError('rpm out of range! -> ' + str(rpm))
 
-                # Parse RPM as a string and strip decimal values
-                rString = str(int(rpm))
-                try:
-                    # Only executes if no errors finding the "00" in rString
-                    test = (rString.index("00") != 2)
+    def _calcBSFC(self, rpm:int, throttle:int)->float:
+        """
+            Method to calculate brake specific fuel consumption
 
-                    # Gets torque at rpm if rpm is divisible by 100
-                    finTorque = self._findTorque(int(rpm / 100), throttle)
+            rpm:      between 1400 and 3600
+            throttle: percentage divisible by 10
+        """
 
-                except:
-                    # The first two digits of RPM (used for torque lookup)
-                    gets = int(rString[0:2])
+        if (rpm >= 1400 and rpm <= 3600):
+            return self._findBSFC(int(rpm / 100), throttle)
+        else:
+            return float(0)
 
-                    # The last two digits of RPM (used for weighting torque multiplcation)
-                    remain = int(rString[2:4])
+    def _calcPower(self, rpm:int, throttle:int)->float:
+        """
+            Method to calculate power
 
-                    # Find and weight the two torque values
-                    t1 = self._findTorque(gets, throttle) * (float(100 - remain) / 100)
-                    t2 = self._findTorque(gets + 1, throttle) * (float(remain) / 100)
+            rpm:      between 1400 and 3600
+            throttle: percentage divisible by 10
+        """
 
-                    # Return the sum of weighted torques
-                    finTorque = t1 + t2
-                return finTorque
+        if (rpm >= 1400 and rpm <= 3600):
+            return self._findPower(int(rpm / 100), throttle)
+        else:
+            return float(0)
 
-    @classmethod
     def _findTorque(self, msbRPM:int, throttle:int)->float:
         """
             Lookup method to find torques in arrays.py
@@ -153,12 +184,13 @@ class SimulateRun:
             msbRPM:   two-digit rpm (rpm / 100)
             thorttle: throttle percentage divisble by 10
         """
-        if (type(msbRPM) != int or type(throttle) != int):
-            raise TypeError('Both parameters must be integers!')
-        else:
-            return self.__findTuple(msbRPM, throttle)[0]
+        # msbRPM = rpm / 100
 
-    @classmethod
+        if ((type(msbRPM) == int) and (type(throttle) == int)):
+            return self.__findTuple(msbRPM, throttle)[0]
+        else:
+            raise TypeError('Type Error with _findTorque() call!')
+    
     def _findPower(self, msbRPM:int, throttle:int)->float:
         """
             Lookup method to find powers in arrays.py
@@ -166,12 +198,13 @@ class SimulateRun:
             msbRPM:   two-digit rpm (rpm / 100)
             thorttle: throttle percentage divisble by 10
         """
-        if (type(msbRPM) != int or type(throttle) != int):
-            raise TypeError('Both parameters must be integers!')
-        else:
-            return self.__findTuple(msbRPM, throttle)[1]
+        # msbRPM = rpm / 100
 
-    @classmethod
+        if ((type(msbRPM) == int) and (type(throttle) == int)):
+            return self.__findTuple(msbRPM, throttle)[1]
+        else:
+            raise TypeError('Type Error with _findPower() call!')
+
     def _findBSFC(self, msbRPM:int, throttle:int)->float:
         """
             Lookup method to find BSFC's in arrays.py
@@ -179,12 +212,13 @@ class SimulateRun:
             msbRPM:   two-digit rpm (rpm / 100)
             thorttle: throttle percentage divisble by 10
         """
-        if (type(msbRPM) != int or type(throttle) != int):
-            raise TypeError('Both parameters must be integers!')
-        else:
-            return self.__findTuple(msbRPM, throttle)[3]
+        # msbRPM = rpm / 100
 
-    @classmethod
+        if ((type(msbRPM) == int) and (type(throttle) == int)):
+            return self.__findTuple(msbRPM, throttle)[2]
+        else:
+            raise TypeError('Type Error with _findBSFC() call!')
+
     def __findTuple(self, msbRPM:int, throttle:int)->tuple:
         """
             Lookup method to find tuples in arrays.py
@@ -192,11 +226,9 @@ class SimulateRun:
             msbRPM:   two-digit rpm (rpm / 100)
             thorttle: throttle percentage divisble by 10
         """
-
         # msbRPM = rpm / 100
-        if (type(msbRPM) != int or type(throttle) != int):
-            raise TypeError('Both parameters must be integers!')
-        else:
+
+        if ((type(msbRPM) == int) and (type(throttle) == int)):
             # Validate RPM Value
             if (msbRPM >= 14 and msbRPM <= 36):
                 # Get Throttle Row based on RPM
@@ -204,27 +236,25 @@ class SimulateRun:
 
                 # Validate Throttle Value to reduce chance of error
                 if ((throttle % 10 == 0) and (throttle > 0 and throttle <= 100)):
-                    # Define array name to retrieve tuple from based on throttle
+                    # Define array name to retrieve torque from based on throttle
                     arrayName = str("t" + str(int(throttle)) + "throttleArray")
 
-                    # Get tuple at certain throttle and RPM
+                    # Get torque at certain throttle and RPM
                     cmd = str(arrayName + "[" + str(index) + "]")
-                    myTuple = eval(cmd)
-                    return myTuple
+                    tup = eval(cmd)
+                    return tup
+                else:
+                    0
+            else:
+                0
+        else:
+            raise TypeError('Type Error with __findTuple() call!')
 
 
-# Simulate Go-Kart Run
+
+# Simulate Go-Kart Runs
 # Generate CSV Files in runs folder
 s = SimulateRun()
 print(s.simulate_run(100, .5, 100))
 print(s.simulate_run(100, .25, 100))
 print(s.simulate_run(100, .05, 100))
-
-
-# Testing for getTorque() and findTorque() functions
-# (No longer needed)
-
-# print("1400rpm @ 100% throttle")
-# print("finalTorque: " + str(s.getTorque(1400, 100)))
-# print("\n1475rpm @ 100% throttle")
-# print("finalTorque: " + str(s.getTorque(1475, 100)))
--- TechClub/fuelConsumption/test_v7.py
+++ TechClub/fuelConsumption/test_v7.py
@@ -12,6 +12,14 @@ import math
 from arrays import *
 
 class SimulateRun:
+    """
+        Class used for modeling Go-Kart runs for WDHS Tech Club
+        
+        This class in used to run the simulate_run() subroutine
+        and has the purpose of optimizing fuel efficiency on
+        a real life go kart for WEEVA competitions for the Western
+        Dubuque High School racing team.
+    """
     
     def __init__(self):
         """
@@ -22,17 +30,17 @@ class SimulateRun:
         """
         
         # Constants
-        self._dLim = 2
-        self._spRatio = 85 / 12
-        self._kMass = 120
-        self._wheelDia = .5588
-        self._outputTorque = 6.9
-        self._dragCoefficent = .3
-        self._frontal = .56
-        self._airDensity = 1.225
-        self._forceTotal = 10
-        self._clutchSlip = 1.0
-        self._drivenWheelCir = math.pi * self._wheelDia
+        self._dLim = 2 # Decimal Delimiter
+        self._spRatio = 85 / 12 # Sprocket Ratio
+        self._kMass = 120 # Kart Mass
+        self._wheelDia = .5588 # Wheel Diameter
+        self._outputTorque = 6.9 # Engine Output Torque (Nm)
+        self._dragCoefficent = .3 # Coefficient of Drag
+        self._frontal = .56 # Kart Frontal Area (m^2)
+        self._airDensity = 1.225 # Air Density (constant)
+        self._forceTotal = 10 # Total of forces acting on kart
+        self._clutchSlip = 1.0 # Clutch slip as percentage
+        self._drivenWheelCir = math.pi * self._wheelDia # Driven Wheel Circumfrance
 
     def simulate_run(self, until:int, step:float, throttle:int)->bool:
         """
@@ -56,9 +64,10 @@ class SimulateRun:
             rpm = 2000
             timeSum = 0
             lockup = False
+            bsfc = 0
 
             # Main Loop
-            for x in range(0, (int)(until * (1 / step) + 1)):
+            for x in range(0, (int)((until / step) + 1)):
 
                 # Calculated
                 kAccel = (((self._outputTorque * self._spRatio * 2) / self._wheelDia) - self._forceTotal - drag) / self._kMass # mph
@@ -67,6 +76,8 @@ class SimulateRun:
                 drag = (velSpeed ** 2) * self._airDensity * self._dragCoefficent * self._frontal / 2 # Drag Coefficient
                 clutchSpeed = velSpeed * 60 * self._spRatio / self._drivenWheelCir 
                 slip = (rpm - clutchSprint) / rpm
+                deltaBSFC = self._calcBSFC(int(rpm), int(throttle)) * self._calcPower(int(rpm), int(throttle)) * step
+                bsfc += deltaBSFC
 
                 # for slip < 0 we need to look up engine speeed using the clutchSpeed. Look up outputTorque == engine torque.   
                 # if lockup == true or
@@ -77,11 +88,10 @@ class SimulateRun:
                     rpm = clutchSpeed
 
                     # Lookup torque value
-                    torque = self._getTorque(rpm, throttle)
-                    print(torque)
+                    torque = self._calcTorque(rpm, throttle)
 
                 # Output
-                output.append([round(timeSum, self._dLim), round(kAccel, self._dLim), round(velSpeed, self._dLim), round(dist, self._dLim), round(slip, self._dLim)])
+                output.append([round(timeSum, self._dLim), round(kAccel, self._dLim), round(velSpeed, self._dLim), round(dist, self._dLim), round(slip, self._dLim), round(bsfc, self._dLim)])
 
                 # Iterate Variables
                 velSprint = velSpeed
@@ -94,16 +104,18 @@ class SimulateRun:
             with open('runs/' + fname, 'w') as csvfile:
                 filewriter = csv.writer(csvfile, delimiter=',',
                                         quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                filewriter.writerow(["Time Step", "Kart Accel", "Vehicle Speed", "Total Distance", "Clutch Slip"])
+                filewriter.writerow(["Time Step", "Kart Accel", "Vehicle Speed", "Total Distance", "Clutch Slip", "BSFC"])
                 for iteration in output:
                     filewriter.writerow(iteration)
             return True
-        except:
+        except Exception as e:
+            raise e
+            print(str(e))
             return False
 
-    def _getTorque(self, rpm:int, throttle:int)->float:
+    def _calcTorque(self, rpm:int, throttle:int)->float:
         """
-            Method to get torque
+            Method to calculate torque
 
             rpm:      between 1400 and 3600
             throttle: percentage divisible by 10
@@ -134,9 +146,36 @@ class SimulateRun:
 
                 # Return the sum of weighted torques
                 return t1 + t2
+        elif (rpm >= 3600):
+            return float(0)
         else:
-            raise LookupError('rpm out of range!')
-            
+            raise LookupError('rpm out of range! -> ' + str(rpm))
+
+    def _calcBSFC(self, rpm:int, throttle:int)->float:
+        """
+            Method to calculate brake specific fuel consumption
+
+            rpm:      between 1400 and 3600
+            throttle: percentage divisible by 10
+        """
+
+        if (rpm >= 1400 and rpm <= 3600):
+            return self._findBSFC(int(rpm / 100), throttle)
+        else:
+            return float(0)
+
+    def _calcPower(self, rpm:int, throttle:int)->float:
+        """
+            Method to calculate power
+
+            rpm:      between 1400 and 3600
+            throttle: percentage divisible by 10
+        """
+
+        if (rpm >= 1400 and rpm <= 3600):
+            return self._findPower(int(rpm / 100), throttle)
+        else:
+            return float(0)
 
     def _findTorque(self, msbRPM:int, throttle:int)->float:
         """
@@ -147,20 +186,69 @@ class SimulateRun:
         """
         # msbRPM = rpm / 100
 
-        # Validate RPM Value
-        if (msbRPM >= 14 and msbRPM <= 36):
-            # Get Throttle Row based on RPM
-            index = int(msbRPM - 14)
+        if ((type(msbRPM) == int) and (type(throttle) == int)):
+            return self.__findTuple(msbRPM, throttle)[0]
+        else:
+            raise TypeError('Type Error with _findTorque() call!')
+    
+    def _findPower(self, msbRPM:int, throttle:int)->float:
+        """
+            Lookup method to find powers in arrays.py
 
-            # Validate Throttle Value to reduce chance of error
-            if ((throttle % 10 == 0) and (throttle > 0 and throttle <= 100)):
-                # Define array name to retrieve torque from based on throttle
-                arrayName = str("t" + str(int(throttle)) + "throttleArray")
+            msbRPM:   two-digit rpm (rpm / 100)
+            thorttle: throttle percentage divisble by 10
+        """
+        # msbRPM = rpm / 100
 
-                # Get torque at certain throttle and RPM
-                cmd = str(arrayName + "[" + str(index) + "][0]")
-                torque = eval(cmd)
-                return torque
+        if ((type(msbRPM) == int) and (type(throttle) == int)):
+            return self.__findTuple(msbRPM, throttle)[1]
+        else:
+            raise TypeError('Type Error with _findPower() call!')
+
+    def _findBSFC(self, msbRPM:int, throttle:int)->float:
+        """
+            Lookup method to find BSFC's in arrays.py
+
+            msbRPM:   two-digit rpm (rpm / 100)
+            thorttle: throttle percentage divisble by 10
+        """
+        # msbRPM = rpm / 100
+
+        if ((type(msbRPM) == int) and (type(throttle) == int)):
+            return self.__findTuple(msbRPM, throttle)[2]
+        else:
+            raise TypeError('Type Error with _findBSFC() call!')
+
+    def __findTuple(self, msbRPM:int, throttle:int)->tuple:
+        """
+            Lookup method to find tuples in arrays.py
+
+            msbRPM:   two-digit rpm (rpm / 100)
+            thorttle: throttle percentage divisble by 10
+        """
+        # msbRPM = rpm / 100
+
+        if ((type(msbRPM) == int) and (type(throttle) == int)):
+            # Validate RPM Value
+            if (msbRPM >= 14 and msbRPM <= 36):
+                # Get Throttle Row based on RPM
+                index = int(msbRPM - 14)
+
+                # Validate Throttle Value to reduce chance of error
+                if ((throttle % 10 == 0) and (throttle > 0 and throttle <= 100)):
+                    # Define array name to retrieve torque from based on throttle
+                    arrayName = str("t" + str(int(throttle)) + "throttleArray")
+
+                    # Get torque at certain throttle and RPM
+                    cmd = str(arrayName + "[" + str(index) + "]")
+                    tup = eval(cmd)
+                    return tup
+                else:
+                    0
+            else:
+                0
+        else:
+            raise TypeError('Type Error with __findTuple() call!')
 
 
 
@@ -170,11 +258,3 @@ s = SimulateRun()
 print(s.simulate_run(100, .5, 100))
 print(s.simulate_run(100, .25, 100))
 print(s.simulate_run(100, .05, 100))
-
-
-# Testing for getTorque() and findTorque() functions
-# (No longer needed)
-# print("1400rpm @ 100% throttle")
-# print("finalTorque: " + str(getTorque(1400, 100)))
-# print("\n1475rpm @ 100% throttle")
-# print("finalTorque: " + str(getTorque(1475, 100)))
